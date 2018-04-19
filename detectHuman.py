67da19b6342f5b8c3be22299aa83ed37124a0602
stefanfeyer <stefan.feyer@uni-konstanz.de>
2018-04-19 18:50:51 +0300

    fill in the methods

--- detectHuman.py
+++ detectHuman.py
@@ -7,14 +7,46 @@ import numpy as np
 import argparse
 import imutils
 import cv2
- 
+
+# initialise variables and parameters
+hog = ''
+hSpeed = 1
+rotSpeed = 1
+moveSpeed = 1
+videoPath = ''
+maxHeight = 1
+maxSpeed = 1
+
+init(1, 1, 1, 'testVideoStefan.mp4')
+#init(1, 1, 1, 'testVideoJogger.mp4')
+initDetectPerson()
+start()
+
+def start():
+    cap = cv2.VideoCapture(videoPath)
+    while(cap.isOpened()):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break  
+    # Capture frame-by-frame
+        ret,frame=cap.read()
+        frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+    
+
+def init(hSpeed, rotSpeed, moveSpeed, videoPath):
+    hSpeed = hSpeed
+    rotSpeed = rotSpeed
+    moveSpeed = moveSpeed
+    videoPath = videoPath
+    return
+
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-videoPath = 'testVideoJogger.mp4'
+
 cap = cv2.VideoCapture(videoPath)
 # take only every ith frame
-ithFrame = 2
+ithFrame = 4
 i=0
 while(cap.isOpened()):
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -25,7 +57,8 @@ while(cap.isOpened()):
 
     if i%ithFrame == 0:
         # detect people in the frame
-        (rects, weights) = hog.detectMultiScale(frameGray, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        #frameGray = imutils.resize(frameGray, width=min(500, frameGray.shape[1]))
+        (rects, weights) = hog.detectMultiScale(frameGray, winStride=(8, 8), padding=(8,8), scale=1.2)
      
         for (x,y,w,h) in rects:
             cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255))
@@ -46,3 +79,47 @@ while(cap.isOpened()):
 cap.release()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# initialize the HOG descriptor/person detector
+def initDetectPerson():
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    return
+
+
+def detectPerson():
+
+    
+    return
+
+
+def drawRectangle():
+    
+    return
+
+# justus
+def calcPersonCenter(rect, imageSize):
+    
+    return
+
+#justus
+def positionDrone():
+    
+    return
+
+#moritz
+
+def turnLeft():
+    return
+def turnRight():
+    return
+def forward():
+    return
+def backward():
+    return
+def up():
+    return
+def down():
+    return
+def land():
+    return
