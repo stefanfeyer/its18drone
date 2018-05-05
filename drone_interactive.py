271b4ce9fb26f95c17862eb0e3600e284e1447c8
m0r13 <moritz.hilscher@gmail.com>
2018-05-05 17:13:52 +0300

    Integrating human detection into drone flying program. Prepare drone state generation

--- drone_interactive.py
+++ drone_interactive.py
@@ -35,21 +35,143 @@ import libardrone
 import cv2
 import numpy as np
 
+FAKE_VIDEO = True
+
+MANUAL_KEYS = [
+    pygame.K_w,
+    pygame.K_s,
+    pygame.K_a,
+    pygame.K_d,
+    pygame.K_UP,
+    pygame.K_DOWN,
+    pygame.K_LEFT,
+    pygame.K_RIGHT,
+    pygame.K_h
+]
+
+W, H = 320, 240
+
+def is_manual_key_pressed():
+    pressed = np.array(list(pygame.key.get_pressed()))
+    pressed = pressed[MANUAL_KEYS]
+    return np.any(pressed != 0)
+
+def get_state_manual(event):
+    if event.type == pygame.KEYUP and event.key in MANUAL_KEYS:
+        return "hover"
+    elif event.type == pygame.KEYDOWN:
+        # forward / backward
+        if event.key == pygame.K_w:
+            return "move_forward"
+        elif event.key == pygame.K_s:
+            return "move_backward"
+        # left / right
+        elif event.key == pygame.K_a:
+            return "move_left"
+        elif event.key == pygame.K_d:
+            return "move_right"
+        # up / down
+        elif event.key == pygame.K_UP:
+            return "move_up"
+        elif event.key == pygame.K_DOWN:
+            return "move_down"
+        # turn left / turn right
+        elif event.key == pygame.K_LEFT:
+            return "turn_left"
+        elif event.key == pygame.K_RIGHT:
+            return "turn_right"
+        # hold drone position (holding also blocks following mode's decisions)
+        elif event.key == pygame.K_h:
+            return "hover"
+    return None
+
+def get_state_following(rect, bounds):
+    if rect is None:
+        return None
+
+    return "hover"
+
+def apply_state(drone, state):
+    states = {
+        "hover" : drone.hover,
+        "move_forward" : drone.move_forward,
+        "move_backward" : drone.move_backward,
+        "move_left" : drone.move_left,
+        "move_right" : drone.move_right,
+        "move_up" : drone.move_up,
+        "move_down" : drone.move_down,
+        "turn_left" : lambda: drone.turn_left(0.5),
+        "turn_right" : lambda: drone.turn_right(0.5),
+        "hold" : lambda: None,
+    }
+    f = states.get(state, None)
+    if f is None:
+        print "### Warning: Unknown state '%s'" % state
+    else:
+        f()
+
+class HumanDetector:
+    def __init__(self):
+        self.hog = cv2.HOGDescriptor()
+        self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+        self.rects = None
+        self.weights = None
+    
+    def process(self, frame):
+        self.rects, self.weights = self.hog.detectMultiScale(frame, winStride=(8, 8), padding=(8,8), scale=1.2)
+
+    def get_rect(self):
+        if self.rects is None or len(self.rects) == 0:
+            return None
+        # or the one with highest weight?
+        return self.rects[np.argmax(self.weights)]
+
+    def render_rects(self, frame):
+        if self.rects is None or len(self.rects) == 0:
+            return frame
+
+        max_weight_i = np.argmax(self.weights)
+        for i, ((x, y, w, h), weight) in enumerate(zip(self.rects, self.weights)):
+            x *= 2
+            y *= 2
+            w *= 2
+            h *= 2
+
+            color = (255, 255, 255) if i != max_weight_i else (255, 0, 0)
+            frame = cv2.rectangle(frame, (x,y), (x+w, y+h), color)
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            frame = cv2.putText(frame, "%.04f" % weight, (x, y - 2), font, 0.5, color, 1, cv2.LINE_8, False)
+        return frame
+
 def main():
     pygame.init()
-    W, H = 320, 240
-    screen = pygame.display.set_mode((W, H))
+    screen = pygame.display.set_mode((W*2, H*2))
     print "Connecting to drone..."
     drone = libardrone.ARDrone()
     print "Connecting to video stream..."
     # video part of api doesn't work
     # but this works:
-    video = cv2.VideoCapture("tcp://192.168.1.1:5555")
+    video = None
+    if not FAKE_VIDEO:
+        video = cv2.VideoCapture("tcp://192.168.1.1:5555")
+    else:
+        video = cv2.VideoCapture("testVideoStefan.mp4")
     print "Done."
     clock = pygame.time.Clock()
+
+    detector = HumanDetector()
+    following = False
+
     running = True
+    last_state = None
     while running:
+        state = None
         for event in pygame.event.get():
+            state = get_state_manual(event)
+            if state is not None:
+                print "Got manual state: %s" % state
             if event.type == pygame.QUIT:
                 running = False 
             elif event.type == pygame.KEYUP:
@@ -66,26 +188,6 @@ def main():
                 # emergency
                 elif event.key == pygame.K_BACKSPACE:
                     drone.reset()
-                # forward / backward
-                elif event.key == pygame.K_w:
-                    drone.move_forward()
-                elif event.key == pygame.K_s:
-                    drone.move_backward()
-                # left / right
-                elif event.key == pygame.K_a:
-                    drone.move_left()
-                elif event.key == pygame.K_d:
-                    drone.move_right()
-                # up / down
-                elif event.key == pygame.K_UP:
-                    drone.move_up()
-                elif event.key == pygame.K_DOWN:
-                    drone.move_down()
-                # turn left / turn right
-                elif event.key == pygame.K_LEFT:
-                    drone.turn_left()
-                elif event.key == pygame.K_RIGHT:
-                    drone.turn_right()
                 # speed
                 elif event.key == pygame.K_1:
                     drone.speed = 0.1
@@ -107,28 +209,47 @@ def main():
                     drone.speed = 0.9
                 elif event.key == pygame.K_0:
                     drone.speed = 1.0
+                elif event.key == pygame.K_p:
+                    pprint.pprint(drone.navdata.get(0, {}))
+                    pprint.pprint(drone.navdata.get("drone_state", {}))
+                elif event.key == pygame.K_f:
+                    following = not following
 
-        #try:
-        #print len(drone.image), W, H, W*H*3
-        #surface = pygame.image.fromstring(drone.image, (W, H), 'RGB')
-        #surface = pygame.Surface((W, H))
-        #print "Waiting for frame"
         ret, frame = video.read()
-        #print "Got frame:", frame.shape
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        surface = pygame.surfarray.make_surface(np.flip(np.rot90(frame), 0))
+        if FAKE_VIDEO:
+            frame = cv2.resize(frame, (W, H))
+
+        detector.process(frame)
+        rect = detector.get_rect()
+
+        if state is None and is_manual_key_pressed():
+            state = "hold"
+        if state is None and following:
+            state = get_state_following(rect, (W, H))
+            if state is not None:
+                print "Got following state: %s" % state
+        if state is not None and state != last_state:
+            print "Applying state: %s" % state
+            apply_state(drone, state)
+        last_state = state
+
+        render_frame = cv2.resize(frame, (int(W*2), int(H*2)), interpolation=cv2.INTER_NEAREST)
+        render_frame = detector.render_rects(render_frame)
+        surface = pygame.surfarray.make_surface(np.flip(np.rot90(render_frame), 0))
         # battery status
         hud_color = (255, 0, 0) if drone.navdata.get('drone_state', dict()).get('emergency_mask', 1) else (10, 10, 255)
+        following_color = (0, 255, 0) if following else (255, 0, 0)
         bat = drone.navdata.get(0, dict()).get('battery', 0)
         f = pygame.font.Font(None, 20)
-        hud = f.render('Battery: %i%%' % bat, True, hud_color)
+        battery_label = f.render('Battery: %i%%' % bat, True, hud_color)
+        following_label = f.render("Following: %s" % following, True, following_color)
         screen.blit(surface, (0, 0))
-        screen.blit(hud, (10, 10))
-        #except:
-        #    pass
+        screen.blit(battery_label, (10, 10))
+        screen.blit(following_label, (10, screen.get_height() - 10 - following_label.get_height()))
 
         pygame.display.flip()
-        clock.tick(50)
+        clock.tick(20)
         pygame.display.set_caption("FPS: %.2f" % clock.get_fps())
 
     print "Shutting down...",
