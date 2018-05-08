d194c7d026d9d08e09adcb470332f08671e12067
stefanfeyer <stefan.feyer@uni-konstanz.de>
2018-05-08 19:03:26 +0300

    bla

--- compareHistograms.py
+++ compareHistograms.py
@@ -8,16 +8,18 @@ from cmath import rect
 from cv2 import calcHist, compareHist, imread, COLOR_RGB2HSV, COLOR_RGB2GRAY
 from operator import add
 import matplotlib.pyplot as plt
+from numpy import shape
 
 
-img1 = cv2.imread("testImages/51.jpg")
-img2 = cv2.imread("testImages/101.jpg")
-img1 = cv2.cvtColor(img1, COLOR_RGB2GRAY)
-img2 = cv2.cvtColor(img2, COLOR_RGB2GRAY)
+img1 = cv2.imread("testImages/2.jpg")
+img2 = cv2.imread("testImages/3.jpg")
+img1 = cv2.cvtColor(img1, COLOR_RGB2HSV)
+img2 = cv2.cvtColor(img2, COLOR_RGB2HSV)
 
 
-hist1 = cv2.calcHist([img1], [0], None, [255], [0,255])
-hist2 = cv2.calcHist([img2], [0], None, [255], [0,255])
+hist1 = cv2.calcHist([img1], [0], None, [180], [0,180])
+hist2 = cv2.calcHist([img2], [0], None, [180], [0,180])
+# normalize
 bhatta = compareHist(hist1, hist2, cv2.HISTCMP_BHATTACHARYYA)
 chi = compareHist(hist1, hist2, cv2.HISTCMP_CHISQR)
 chi_alt = compareHist(hist1, hist2, cv2.HISTCMP_CHISQR_ALT)
@@ -25,9 +27,27 @@ correl = compareHist(hist1, hist2, cv2.HISTCMP_CORREL)
 hellinger = compareHist(hist1, hist2, cv2.HISTCMP_HELLINGER)
 intersect = compareHist(hist1, hist2, cv2.HISTCMP_INTERSECT)
 kl_div = compareHist(hist1, hist2, cv2.HISTCMP_KL_DIV)
-emd = cv2.EMDHistogramCostExtractor(hist1, hist2, cv2.DIST_L1)
+#print chi, chi_alt, correl, hellinger, intersect, kl_div
+#print hist1.__class__
+#print hist1.shape
+hist1_indexed = np.zeros((len(hist1), 2), np.float32)
+hist1_indexed[:,0] = hist1[:, 0]
+hist1_indexed[:,1] = range(0,len(hist1))
+
+hist2_indexed = np.zeros((len(hist2), 2), np.float32)
+hist2_indexed[:,0] = hist2[:, 0]
+hist2_indexed[:,1] = range(0,len(hist2))
+
+#hist1.resize((len(hist1), 2))
+#hist1 = np.resize(hist1, (len(hist1), 2))
+#print hist1
+#newHist1 = np.zeros((len(hist1), 2))
+#newHist1[:,0] = hist1[0]
+#print newHist1
+
+emd = cv2.EMD(hist1_indexed, hist2_indexed, cv2.DIST_L1)
 print emd
-print chi, chi_alt, correl, hellinger, intersect, kl_div
+
 
 
 plt.figure(0)
